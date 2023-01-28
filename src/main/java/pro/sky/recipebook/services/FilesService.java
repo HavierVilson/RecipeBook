6443e9b289b0a5da8d0370d08a02ba3335cdb4ab
HavierVislon <haviervilson@gmail.com>
2023-01-28 17:38:29 +0300

    homework

--- src/main/java/pro/sky/recipebook/services/FilesService.java
+++ src/main/java/pro/sky/recipebook/services/FilesService.java
@@ -1,5 +1,7 @@
 package pro.sky.recipebook.services;
 
+import java.io.File;
+
 public interface FilesService {
     boolean saveToIngredientsFile(String json);
 
@@ -12,4 +14,8 @@ public interface FilesService {
     boolean cleanIngredientsFile();
 
     boolean cleanRecipesFile();
+
+    File getIngredientsFile();
+
+    File getRecipesFile();
 }
--- src/main/java/pro/sky/recipebook/services/impl/FilesServiceImpl.java
+++ src/main/java/pro/sky/recipebook/services/impl/FilesServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import pro.sky.recipebook.services.FilesService;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
@@ -94,5 +95,15 @@ public class FilesServiceImpl implements FilesService {
             e.printStackTrace();
             return false;
         }
+    }
+
+    @Override
+    public File getIngredientsFile() {
+        return new File(dataFilePath + "/" + ingredientsFileName);
+    }
+
+    @Override
+    public File getRecipesFile() {
+        return new File(dataFilePath + "/" + recipesFileName);
     }
 }
